1bea3294d9eca0470212dc6d5655a8aad113f15e
Ruslan <115349782+khesea@users.noreply.github.com>
2023-03-04 05:39:43 +0300

    timer, flags, smile

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const field = document.querySelector('.field');
+const thirdDigit = document.querySelector('.time_third_digit');
+const secondDigit = document.querySelector('.time_second_digit');
+const firstDigit = document.querySelector('.time_first_digit');
+let number = 0;
+let secondNum = 0;
 startGame(16, 16, 40);
 
 function startGame(WIDTH, HEIGHT, BOMBS_COUNT) {
@@ -13,7 +18,7 @@ function startGame(WIDTH, HEIGHT, BOMBS_COUNT) {
     .slice(0, BOMBS_COUNT);
 
     for (let i = 0; i < 40; i++) {
-      cells[bombs[i]].innerHTML = '<div class="mine"></div>';
+      cells[bombs[i]].classList.add('mine');
     }
     const mines = field.querySelectorAll('.mine');
 
@@ -27,6 +32,42 @@ function startGame(WIDTH, HEIGHT, BOMBS_COUNT) {
     const row = Math.floor(index / WIDTH);
     open(row, column);
   });
+
+  field.addEventListener('click', setTimer, false);
+  function setTimer() {
+    intervalID1 = setInterval(function () {
+      number++;
+      if (number<1000) {
+        let thirdNumber = number % 10;
+        thirdDigit.style.cssText = `background-image: url(images/d${thirdNumber}.svg)`;
+
+        let firstNumber = Math.floor(number/100);
+        firstDigit.style.cssText = `background-image: url(images/d${firstNumber}.svg)`;
+      }
+    }, 1000);
+
+    intervalID2 = setInterval(function () {
+      secondNum++;
+      if (secondNum<100) {
+        let secondNumber = secondNum % 10;
+        secondDigit.style.cssText = `background-image: url(images/d${secondNumber}.svg)`;
+      }
+    }, 10000);
+    field.removeEventListener('click', setTimer, false);
+  }
+
+  const smile = document.querySelector('.smile_reaction');
+  field.addEventListener('mousedown', changeStyle, false);
+  function changeStyle(e) {
+    smile.classList.remove('smile');
+    smile.classList.add('wow');
+  }
+
+  field.addEventListener('mouseup', changeStyleBack, false);
+  function changeStyleBack() {
+    smile.classList.add('smile');
+    smile.classList.remove('wow');
+  }
 
   function isValid(row, column) {
     return row >= 0
@@ -58,19 +99,26 @@ function startGame(WIDTH, HEIGHT, BOMBS_COUNT) {
     cell.disabled = true;
     cell.style.border = '1px solid #7B7B7B';
     if (isBomb(row, column)) {
-      cell.style.backgroundColor = 'red';
       for (let i = 0; i < 40; i++) {
-        mines[i].style.display = 'block';
+        mines[i].style.cssText = 'background-image: url(images/mine.png);';
       }
+      cell.style.cssText = 'background-image: url(images/mine.png);';
+      cell.style.cssText = 'background-color: red;';
       for (let i = 0; i < cells.length; i++) {
         cells[i].disabled = true;
       }
+      smile.classList.remove('smile');
+      smile.classList.add('lose');
+      field.removeEventListener('contextmenu', setFlag, false);
+      clearInterval(intervalID1);
+      clearInterval(intervalID2);
       return;
     }
 
     closedCount--;
     if (closedCount <= BOMBS_COUNT) {
-      alert('you won');
+      smile.classList.remove('smile');
+      smile.classList.add('lose');
       return;
     }
 
@@ -118,5 +166,33 @@ function startGame(WIDTH, HEIGHT, BOMBS_COUNT) {
     const index = row * WIDTH + column;
 
     return bombs.includes(index);
+  }
+
+  const flags = 40;
+  const thirdMine = document.querySelector('.mines_third_digit');
+  const secondMine = document.querySelector('.mines_second_digit');
+  field.addEventListener('contextmenu', setFlag, false);
+  function setFlag(el) {
+    el.preventDefault();
+    if (el.target != el.currentTarget) {
+      let clickedItem = el.target;
+      if (clickedItem.disabled == false) {
+        clickedItem.classList.toggle('flag');
+      }
+    }
+
+    const flagsTile = document.querySelectorAll('.flag');
+    let thirdFlag = (flags - flagsTile.length) % 10;
+    let secondFlag = Math.floor((flags - flagsTile.length)/10);
+
+    if ((flags - flagsTile.length)>=0) {
+      thirdMine.style.cssText = `background-image: url(images/d${thirdFlag}.svg)`;
+      secondMine.style.cssText = `background-image: url(images/d${secondFlag}.svg)`;
+      }
+    }
+
+  smile.addEventListener('click', newGame, false);
+  function newGame() {
+    location.reload();
   }
 }
